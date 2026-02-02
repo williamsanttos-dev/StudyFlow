f213f0dca66faecbc44c2c951748a4d8df4cdd0d
William01br <williamstcorreia123@gmail.com>
2026-02-02 14:52:07 -0300

    feat: add schemas zod and preValidation in topic endpoints

--- src/modules/topics/update-topic.handler.ts
+++ src/modules/topics/update-topic.handler.ts
@@ -1,8 +1,14 @@
 import type { FastifyRequest, FastifyReply } from 'fastify'
+import { TopicRequestSchema } from './topic.schema'
+import { app } from '@/app'
 
 export async function updateTopicHandler(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
+  // não quero aceitar payload vazio
+  const data = TopicRequestSchema.safeParse(request.body)
+  if (!data.success) throw app.httpErrors.badRequest('Invalid payload')
+
   return reply.status(200).send({ message: 'not implemented yet' })
 }
