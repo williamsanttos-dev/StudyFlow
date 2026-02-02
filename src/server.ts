ed27860e0ae38d50562a3e9a6e8029c9b7f68369
William01br <williamstcorreia123@gmail.com>
2026-02-02 08:23:14 -0300

    chore: register all routes in application

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,7 @@
-import { fastify } from 'fastify'
-import {
-  serializerCompiler,
-  validatorCompiler,
-  jsonSchemaTransform,
-  type ZodTypeProvider,
-} from 'fastify-type-provider-zod'
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifyCors } from '@fastify/cors'
-import ScalarApiReference from '@scalar/fastify-api-reference'
-
-const app = fastify().withTypeProvider<ZodTypeProvider>()
-
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
-
-app.register(fastifyCors, {
-  origin: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  // credentials: true,
-  // credentials or JWT sessions?
-})
-
-app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'Study Flow API',
-      description: 'API for monitor progress in studies',
-      version: '1.0.0',
-    },
-  },
-  transform: jsonSchemaTransform,
-})
-
-app.register(ScalarApiReference, {
-  routePrefix: '/docs',
-})
+import { app } from './app'
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
+  console.log('Health check available at http://localhost:3333/health')
   console.log('Docs available at http://localhost:3333/docs')
 })
