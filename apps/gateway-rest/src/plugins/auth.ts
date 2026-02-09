9f424c56a65439a58d31258b7122bc8b607fd427
William01br <williamstcorreia123@gmail.com>
2026-02-09 07:38:47 -0300

    refactor: refactor auth plugin for improve validation of access token

--- apps/gateway-rest/src/plugins/auth.ts
+++ apps/gateway-rest/src/plugins/auth.ts
@@ -1,14 +1,40 @@
-import fp from 'fastify-plugin'
-import type { FastifyPluginAsync, FastifyInstance } from 'fastify'
+import fp from "fastify-plugin";
+import type { FastifyPluginAsync, FastifyInstance } from "fastify";
+import * as jwt from "jsonwebtoken";
+
+interface AuthPayload extends jwt.JwtPayload {
+	userId: string;
+}
 
 export const authPlugin: FastifyPluginAsync = fp(
-  async (fastify: FastifyInstance) => {
-    fastify.decorateRequest('user', null)
+	async (fastify: FastifyInstance) => {
+		fastify.decorateRequest("user", null);
 
-    fastify.addHook('preHandler', async (request, reply) => {
-      request.user = {
-        userId: 'fake-user-id',
-      }
-    })
-  },
-)
+		fastify.addHook("preValidation", async (request) => {
+			const accessToken =
+				request.cookies.access_token ??
+				request.headers.authorization?.split(" ")[1];
+
+			if (!accessToken)
+				throw fastify.httpErrors.unauthorized("TOKEN_NOT_FOUND");
+
+			try {
+				const payload = jwt.verify(
+					accessToken,
+					process.env.SECRET_ACCESS_TOKEN,
+				) as AuthPayload;
+
+				request.user = {
+					userId: payload.userId,
+				};
+			} catch (err) {
+				if (err instanceof jwt.JsonWebTokenError) {
+					console.error("JsonWebToken err:", err);
+					throw fastify.httpErrors.unauthorized("INVALID_TOKEN");
+				}
+				console.error("INTERNAL_SERVER_ERROR:", err);
+				throw fastify.httpErrors.internalServerError("INTERNAL_SERVER_ERROR");
+			}
+		});
+	},
+);
